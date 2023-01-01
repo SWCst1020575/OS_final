7f4bd75b9991f0414be9b3fe118cbbc27798e01f
SWC <st1020575@gmail.com>
2023-01-02 03:59:38 +0800

    Update HW3.

---
+++ hw3/preemptive.h
@@ -0,0 +1,41 @@
+/*
+ * file: preemptive.h (modified from cooperative.h)
+ */
+
+#ifndef __PREEMPTIVE_H__
+#define __PREEMPTIVE_H__
+
+#define MAXTHREADS 4 /* not including the scheduler */
+/* the scheduler does not take up a thread of its own */
+
+#define CNAME(s) _ ## s
+#define LABEL(label) label ## $
+
+
+#define SemaphoreWaitBody(s, label){ 	\
+	__asm							 	\
+		label:	MOV ACC, CNAME(s)		\
+				JZ  label				\
+	   			dec  CNAME(s) 			\
+	__endasm;							\
+}
+
+// signal() semaphore s
+#define SemaphoreSignal(s){		\
+	__asm 						\
+		INC CNAME(s)			\
+	__endasm;					\
+}
+
+typedef char ThreadID;
+typedef void (*FunctionPtr)(void);
+
+void SemaphoreCreate(char *, char);
+
+ThreadID ThreadCreate(FunctionPtr);
+void ThreadYield(void);
+void ThreadExit(void);
+void myTimer0Handler();
+
+
+#endif
---
+++ hw3/testpreempt.c
@@ -0,0 +1,102 @@
+/*
+ * file: testcoop.c
+ */
+#include <8051.h>
+
+#include "preemptive.h"
+
+__data __at(0x3A) char currentChar;
+__data __at(0x3B) char shared;
+__data __at(0x3C) char full;
+__data __at(0x3D) char mutex;
+__data __at(0x3E) char empty;
+
+void Producer(void) {
+    currentChar = 'A';
+    while (1) {
+        // don't need yield
+        /*if(buffer)
+                ThreadYield();*/
+        /*
+        if (!buffer) {
+            shared = currentChar;
+            if (currentChar == 'Z')
+                currentChar = 'A';
+            else
+                currentChar++;
+            buffer = 1;
+            // ThreadYield();
+        }
+        */
+        SemaphoreWaitBody(empty, LABEL(__COUNTER__) );
+        SemaphoreWaitBody(mutex, LABEL(__COUNTER__) );
+        __critical{
+            shared = currentChar;
+            if (currentChar == 'Z')
+                currentChar = 'A';
+            else
+                currentChar++;
+        }
+        SemaphoreSignal(mutex);
+        SemaphoreSignal(full);
+    }
+}
+
+void Consumer(void) {
+
+    
+    TMOD = 0x20;
+    TH1 = -6;
+    SCON = 0x50;
+    TR1 = 1;
+    TI = 1;
+    while (1) {
+        // don't need yield
+        /*if (!buffer)
+                ThreadYield();*/
+        /*
+        if (buffer) {
+            // polling
+            while (!TI) {
+            }
+            SBUF = shared;
+            TI = 0;
+            buffer = 0;
+            // ThreadYield();
+        }
+        */
+        SemaphoreWaitBody(full,  LABEL(__COUNTER__));
+        SemaphoreWaitBody(mutex,  LABEL(__COUNTER__));
+        __critical{
+            SBUF = shared;
+        }
+        SemaphoreSignal(mutex);
+        SemaphoreSignal(empty);
+        // polling
+        while( !TI ){}
+        TI = 0;
+    }
+}
+
+void main(void) {
+    SemaphoreCreate(&full, 0);
+    SemaphoreCreate(&mutex, 1);
+    SemaphoreCreate(&empty,1);
+    ThreadCreate(Producer);
+    Consumer();
+}
+
+void _sdcc_gsinit_startup(void) {
+    __asm
+		ljmp _Bootstrap
+	__endasm;
+}
+
+void _mcs51_genRAMCLEAR(void) {}
+void _mcs51_genXINIT(void) {}
+void _mcs51_genXRAMCLEAR(void) {}
+void timer0_ISR(void) __interrupt(1) {
+    __asm
+		ljmp _myTimer0Handler
+	__endasm;
+}
